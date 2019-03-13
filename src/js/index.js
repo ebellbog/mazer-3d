05a453602076b34cc1fa841b3d9fadcd16e1ded5
Elana Bell Bogdan <elana@hospiq.com>
2019-03-13 17:54:14 -0500

    Make maze prettier (rounded corners, gradient color, respond to window resize)

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,21 @@ import Maze from './Maze.js';
 import Wall from './Wall.js'
 import {WallState} from './Wall.js'
 
-const nRows = 10;
-const nCols = 10;
+const nRows = 15;
+const nCols = 15;
 
 $(document).ready(() => {
     const maze = new Maze(nRows, nCols);
-    maze.generateMaze();
-    //TESTING
-    // const walls = maze.data.reduce((cells, row) => {
-    //     return cells.concat(row.filter((entity) => entity instanceof Wall));
-    // }, []);
-    // const remove = [10, 20, 30, 40, 50, 60, 70, 80, 90];
-    // remove.forEach(number => walls[number].state = WallState.REMOVED);
+    setupView(maze);
+    window.onresize = () => updateView(maze);
 
-    //END TESTING
-    displayMaze(maze);
+    maze.generateMaze();
+    renderMaze(maze);
 });
 
-function displayMaze(maze) {
+function setupView(maze) {
     const maze$ = $('#maze');
+
     const templates$ = $('#templates');
     const cell$ = templates$.find('.cell');
 
@@ -29,30 +25,73 @@ function displayMaze(maze) {
     const mazeCols = maze.cols;
     const mazeRows = maze.rows;
 
-    cells.forEach((cell) => {
+    cells.forEach((cell, idx) => {
         const newCell$ = cell$.clone();
-
-        const wallDict = cell.getWalls();
-        Object.keys(wallDict).forEach((key) => {
-            if (wallDict[key].state === WallState.REMOVED){
-                newCell$.find('.walls').addClass(`open-${key}`);
-            }
-        })
-
+        newCell$.attr('data-cell-idx', idx);
         maze$.append(newCell$);
     });
 
     maze$.css({
         'grid-template-rows': `repeat(${mazeRows}, 1fr)`,
-        'grid-template-columns': `repeat(${mazeCols}, 1fr)`,
-        width: mazeCols > mazeRows ? '80vw' : `${80 * mazeCols / mazeRows}vh` ,
-        height: mazeRows >= mazeCols ? '80vh' : `${80 * mazeRows / mazeCols}vw` ,
+        'grid-template-columns': `repeat(${mazeCols}, 1fr)`
     });
 
+    updateView(maze);
+}
 
-    const firstCell$ = maze$.find('.cell').first();
-    const firstWall$ = firstCell$.find('.walls');
+function updateView(maze) {
+    const maze$ = $('#maze');
+    const mazeBg$ = $('#maze-background');
 
-    const mazePadding = (firstCell$.width()-firstWall$.width())/2;
-    maze$.css('padding', mazePadding);
+    const cells = maze.getCells()
+    const mazeCols = maze.cols;
+    const mazeRows = maze.rows;
+
+    mazeBg$.css({
+        width: mazeCols > mazeRows ? '80vw' : `${80 * mazeCols / mazeRows}vh` ,
+        height: mazeRows >= mazeCols ? '80vh' : `${80 * mazeRows / mazeCols}vw`
+    });
+
+    const defaultCell$ = maze$.find('.cell:not(.open-left):not(.open-right)').first();
+    const defaultWall$ = defaultCell$.find('.walls');
+    const mazePadding = (defaultCell$.width()-defaultWall$.width())/2;
+
+    mazeBg$.css('padding', mazePadding);
+
+    $('.corner').css({
+        height: mazePadding*2,
+        width: mazePadding*2,
+        'border-radius': mazePadding
+    });
+    $('.corner.top-left').css({
+        top: -mazePadding,
+        left: -mazePadding
+    });
+    $('.corner.top-right').css({
+        top: -mazePadding,
+        right: -mazePadding
+    });
+    $('.corner.bottom-left').css({
+        bottom: -mazePadding,
+        left: -mazePadding
+    });
+    $('.corner.bottom-right').css({
+        bottom: -mazePadding,
+        right: -mazePadding
+    });
+}
+
+function renderMaze(maze) {
+    const cells = maze.getCells();
+
+    cells.forEach((cell, idx) => {
+        const cell$ = $(`.cell[data-cell-idx=${idx}]`);
+        const wallDict = cell.getWalls();
+
+        Object.keys(wallDict).forEach((key) => {
+            if (wallDict[key].state === WallState.REMOVED){
+                cell$.addClass(`open-${key}`);
+            }
+        })
+    });
 }
